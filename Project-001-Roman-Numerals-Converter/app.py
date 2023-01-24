7e540e8ef9a0238a22abafd6993d9ad5f947621f
abdullahsahin27 <abdullahsahin2017@gmail.com>
2023-01-23 21:17:38 -0500

    code changed

--- Project-001-Roman-Numerals-Converter/app.py
+++ Project-001-Roman-Numerals-Converter/app.py
@@ -14,17 +14,17 @@ def main_post():
     if request.method == 'POST':
         alpha = str(request.form['number'])
         if not alpha.isdecimal():
-            return render_template('index.html', developer_name='Altaz', not_valid=True)
+            return render_template('index.html', developer_name='Abdullah', not_valid=True)
         else:
             number = int(alpha)
             if not 0 < number < 4000:
-                return render_template('index.html', developer_name='Altaz', not_valid=True)
+                return render_template('index.html', developer_name='Abdullah', not_valid=True)
             else:
                 roman_number = 'X'
                 return render_template('result.html', number_decimal=number, number_roman=roman_number, developer_name='Altaz')
     else:
-        return render_template('index.html', developer_name='Altaz', not_valid=False)
+        return render_template('index.html', developer_name='Abdullah', not_valid=False)
     
-    if __name__ == '__main__':
-        #app.run(debug=True)
-        app.run(host='0.0.0.0', port=80)
+if __name__ == '__main__':
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
